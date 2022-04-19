73430066d06000f47094044c163409c166ca0a08
Yarynochka <jaryna.games@gmail.com>
2022-04-19 14:39:57 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,104 @@
+//task 1
+// let x= prompt('enter the number');
+// if (x==='0'){
+//     console.log('true');
+// }
+// else{
+//     console.log('false');
+// }
+
+
+
+
+
+//task 2.1
+// let time=prompt('Введіть хвилини години від 1 до 59');
+// if(time>=1 && time<=15){
+//     console.log('Це перша чверть години');
+// }
+// else if( time>=16 && time<=30){
+//     console.log('Це друга чверть години');
+// }
+//
+// else if( time>=31 && time<=45){
+//     console.log('Це третя чверть години');
+// }
+// else if( time>=46 && time<60){
+//     console.log('Це четверта чверть години');
+// }
+// else {
+//     console.log('Ви ввели невірне значення')
+// }
+
+//task 2.2
+// let date=prompt('Введіть число місяця від 1 до 31');
+// if(date>=1 && date<=10){
+//     console.log('Це перша декада місяця');
+// }
+// else if( date>=11 && date<=20){
+//     console.log('Це друга декада місяця');
+// }
+//
+// else if( date>=21 && date<=31){
+//     console.log('Це третя декада місяця');
+// }
+// else {
+//     console.log('Ви ввели невірне значення')
+// }
+
+
+
+
+//task 3
+// switch (+prompt('введіть порядковий номер дня тижня')){
+//     case +'1':
+//         console.log('monday');
+//         break;
+//     case +'2':
+//         console.log('tuesday');
+//         break;
+//     case +'3':
+//         console.log('wednesday');
+//         break;
+//     case +'4':
+//         console.log('thursday');
+//         break;
+//     case +'5':
+//         console.log('friday');
+//         break;
+//     case +'6':
+//         console.log('saturday');
+//         break;
+//     case +'7':
+//         console.log('sunday');
+//         break;
+//     default:
+//         console.log('Ви ввели невірне значення')
+// }
+
+
+
+
+// task 4
+// let a=+prompt('Введіть перше число');
+// let b=+prompt('Введіть друге число');
+// //way1
+// if(a>b){
+//     console.log(a+' є більше за '+b);
+// }
+// else if ( a<b){
+//     console.log(a+' є менше за '+b);
+// }
+// else {
+//     console.log(a+' дорівнює '+b);
+// }
+//way 2
+//  if(a>b){
+//      console.log(a);
+//  }
+//  else if(b>a){
+//      console.log(b)
+//  }
+//  else{
+//      console.log(a+ ' = '+b);
+//  }
